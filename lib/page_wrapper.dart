59ee4695c02c0725f1be68bb51b898f364f54803
Nagellan <i.nazmiev@eora.ru>
2020-11-07 22:02:32 +0300

    Move more layout parts to PageWrapper

--- lib/page_wrapper.dart
+++ lib/page_wrapper.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 
 class PageWrapper extends StatelessWidget {
+  final String title;
   final Widget child;
 
-  PageWrapper({this.child});
+  PageWrapper({this.title, this.child});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      child: Center(
-        child: Card(
-          color: Colors.white,
-          child: this.child,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Card(
+            color: Colors.white,
+            child: this.child,
+          ),
         ),
       ),
     );
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -12,61 +12,55 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login page'),
-        centerTitle: true,
-        backgroundColor: Colors.deepOrangeAccent,
-      ),
-      body: PageWrapper(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Form(
-            key: loginFormKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Login',
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please fill this field';
+    return PageWrapper(
+      title: 'Login page',
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Form(
+          key: loginFormKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Login',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please fill this field';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please fill this field';
+                  }
+                  if (value.length < 8) {
+                    return 'Password must have at least 8 letters';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (loginFormKey.currentState.validate()) {
+                      // TODO: remove print
+                      print('LOGIN SUCCESS');
+                    } else {
+                      // TODO: remove print
+                      print('LOGIN INCORRECT');
                     }
-                    return null;
                   },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please fill this field';
-                    }
-                    if (value.length < 8) {
-                      return 'Password must have at least 8 letters';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      if (loginFormKey.currentState.validate()) {
-                        // TODO: remove print
-                        print('LOGIN SUCCESS');
-                      } else {
-                        // TODO: remove print
-                        print('LOGIN INCORRECT');
-                      }
-                    },
-                    child: Text('Login')),
-              ],
-            ),
+                  child: Text('Login')),
+            ],
           ),
         ),
       ),
