e109865f383545eb0d17b7122f5e18ab870e4fe0
Nagellan <i.nazmiev@eora.ru>
2020-11-07 22:02:52 +0300

    Add SignUp route and basic Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:assignment_1/pages/login.dart';
+import 'package:assignment_1/pages/sign_up.dart';
 
 void main() {
   runApp(MaterialApp(
     routes: {
       '/': (context) => Login(),
+      '/sign-up': (context) => SignUp(),
     },
   ));
 }
