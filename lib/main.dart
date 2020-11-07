5062458369eaea1cc2ab7f5522f4bfaf59b06195
Nagellan <i.nazmiev@eora.ru>
2020-11-07 21:19:54 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:assignment_1/pages/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    routes: {
+      '/': (context) => Login(),
+    },
+  ));
+}
