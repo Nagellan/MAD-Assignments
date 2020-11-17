1b942ea1e43c5f811cf70619e4d18c4ca584b511
Nagellan <ireknazmievirek@gmail.com>
2020-11-17 23:27:38 +0300

    Add pokeApi request

--- lib/pages/pokedex/pokedex.dart
+++ lib/pages/pokedex/pokedex.dart
@@ -1,7 +1,10 @@
+import 'dart:convert' as convert;
+
 import 'package:assignment_2/pages/pokedex/app_bar.dart';
 import 'package:assignment_2/pages/pokedex/grid.dart';
 import 'package:assignment_2/pages/pokedex/search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Pokedex extends StatefulWidget {
   @override
@@ -9,8 +12,31 @@ class Pokedex extends StatefulWidget {
 }
 
 class _PokedexState extends State<Pokedex> {
-  void handleSubmit(String text) {
-    print(text);
+  List<String> pokemons = [];
+
+  void addPokemonForms(var pokemonForms) {
+    if (pokemonForms == null) return;
+    pokemonForms.forEach((pokemonForm) {
+      String pokemonName = pokemonForm['species']['name'];
+      setState(() {
+        pokemons.add(pokemonName);
+      });
+      addPokemonForms(pokemonForm['evolves_to']);
+    });
+  }
+
+  void handleSubmit(String text) async {
+    Response pokemonSpeciesRes =
+    await get('https://pokeapi.co/api/v2/pokemon-species/$text');
+    if (pokemonSpeciesRes.statusCode == 200) {
+      var pokemonSpecies = convert.jsonDecode(pokemonSpeciesRes.body);
+      String evolutionChainUrl = pokemonSpecies['evolution_chain']['url'];
+      Response evolutionChainRes = await get(evolutionChainUrl);
+      if (evolutionChainRes.statusCode == 200) {
+        var evolutionChain = convert.jsonDecode(evolutionChainRes.body);
+        addPokemonForms(evolutionChain['chain']['evolves_to']);
+      }
+    }
   }
 
   @override
@@ -19,7 +45,7 @@ class _PokedexState extends State<Pokedex> {
       backgroundColor: Color(0xffeef8ff),
       appBar: PokeAppBar(),
       body: Stack(children: <Widget>[
-        Grid(pokemons: []),
+        Grid(pokemons: pokemons),
         Align(
           alignment: Alignment.bottomCenter,
           child: SearchBar(handleSubmit: handleSubmit),
