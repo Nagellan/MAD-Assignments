7fee8829ddc434a290ff98e9c910f926c0be5931
Nagellan <ireknazmievirek@gmail.com>
2020-11-17 22:23:46 +0300

    Fix PokeAppBar

--- lib/pages/pokedex/app_bar.dart
+++ lib/pages/pokedex/app_bar.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 
 class PokeAppBar extends AppBar {
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      toolbarHeight: 70,
-      title: Text(
-        'Pokedex',
-        style: TextStyle(
-          color: Color(0xff222222),
-          fontFamily: 'PF-Benchmark-Pro-Bold',
-          fontSize: 25.0,
-        ),
+  PokeAppBar()
+      : super(
+    toolbarHeight: 70,
+    title: Text(
+      'Pokedex',
+      style: TextStyle(
+        color: Color(0xff222222),
+        fontFamily: 'PF-Benchmark-Pro-Bold',
+        fontSize: 25.0,
       ),
-      centerTitle: true,
-      backgroundColor: Colors.transparent,
-      shadowColor: Colors.transparent,
-    );
-  }
+    ),
+    centerTitle: true,
+    backgroundColor: Colors.transparent,
+    shadowColor: Colors.transparent,
+  );
 }
