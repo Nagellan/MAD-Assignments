88b23cb0709979612746a4193e3c936afe505eef
Nagellan <ireknazmievirek@gmail.com>
2020-12-06 17:01:34 +0300

    Add Filter values handling

--- lib/filter.dart
+++ lib/filter.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
-class Filter extends StatelessWidget {
+class Filter extends StatefulWidget {
+  @override
+  _FilterState createState() => _FilterState();
+}
+
+class _FilterState extends State<Filter> {
+  List<String> kinds = ['Any', 'One', 'Two', 'Three', 'Four'];
+  List<String> breeds = ['Any', 'One', 'Two', 'Three', 'Four'];
+
+  String currentKind = 'Any';
+  String currentBreed = 'Any';
+
+  void handleChange({String kind, String breed}) {
+    currentKind = kind == null ? currentKind : kind;
+    currentBreed = breed == null ? currentBreed : breed;
+
+    print('$currentKind $currentBreed');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -11,16 +29,52 @@ class Filter extends StatelessWidget {
           borderRadius: BorderRadius.circular(10.0),
         ),
         child: Padding(
-          padding: EdgeInsets.all(10),
+          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
           child: Row(
             children: <Widget>[
               Expanded(
                 flex: 1,
-                child: Text('First'),
+                child: DropdownButtonFormField<String>(
+                  value: currentKind,
+                  onChanged: (String value) => handleChange(kind: value),
+                  items: kinds
+                      .map<DropdownMenuItem<String>>(
+                        (String value) => DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        ),
+                      )
+                      .toList(),
+                  decoration: InputDecoration(
+                    labelText: 'Kind',
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              Container(
+                height: 40,
+                child: VerticalDivider(
+                  width: 30,
+                ),
               ),
               Expanded(
                 flex: 1,
-                child: Text('Second'),
+                child: DropdownButtonFormField<String>(
+                  value: currentBreed,
+                  onChanged: (String value) => handleChange(breed: value),
+                  items: breeds
+                      .map<DropdownMenuItem<String>>(
+                        (String value) => DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        ),
+                      )
+                      .toList(),
+                  decoration: InputDecoration(
+                    labelText: 'Breed',
+                    border: InputBorder.none,
+                  ),
+                ),
               )
             ],
           ),
